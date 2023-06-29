5820f2fd1e948785b56bc15e4750d099cd27bd98
Leonardo Barbosa <leonardo.sbarbosa2801@gmail.com>
2023-06-29 09:32:51 -0300

    first commit

---
+++ src/main/java/com/leonardosbarbosa/easyposts/EasypostsApplication.java
@@ -0,0 +1,13 @@
+package com.leonardosbarbosa.easyposts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasypostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasypostsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardosbarbosa/easyposts/EasypostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardosbarbosa.easyposts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasypostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
