71b5248a9650192b4f89c5260362c34ab7de7a69
Leonardo Barbosa <leonardo.sbarbosa2801@gmail.com>
2023-06-29 16:35:41 -0300

    Database seed

--- src/main/java/com/leonardosbarbosa/easyposts/config/TestConfig.java
+++ src/main/java/com/leonardosbarbosa/easyposts/config/TestConfig.java
@@ -1,5 +1,8 @@
 package com.leonardosbarbosa.easyposts.config;
 
+import com.leonardosbarbosa.easyposts.models.embedded.Author;
+import com.leonardosbarbosa.easyposts.models.embedded.Comment;
+import com.leonardosbarbosa.easyposts.models.entities.Post;
 import com.leonardosbarbosa.easyposts.models.entities.User;
 import com.leonardosbarbosa.easyposts.repositories.PostRepository;
 import com.leonardosbarbosa.easyposts.repositories.UserRepository;
@@ -7,7 +10,8 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import java.util.List;
+import java.time.Instant;
+import java.util.Arrays;
 
 @Configuration
 @Profile("test")
@@ -26,9 +30,25 @@ public class TestConfig {
         userRepository.deleteAll();
         postRepository.deleteAll();
 
-        User leonardo = new User(null, "Leonardo Barbosa", "leonardo@email.com");
-        User maria = new User(null, "Maria Brown", "maria@email.com");
-        User bob = new User(null, "Bob grey", "bob@email.com");
-        userRepository.saveAll(List.of(leonardo, maria, bob));
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, Instant.parse("2021-02-13T11:15:01Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new Author(maria));
+        Post post2 = new Post(null, Instant.parse("2021-02-14T10:05:49Z"), "Bom dia", "Acordei feliz hoje!", new Author(maria));
+
+        Comment c1 = new Comment("Boa viagem mano!", Instant.parse("2021-02-13T14:30:01Z"), new Author(alex));
+        Comment c2 = new Comment("Aproveite", Instant.parse("2021-02-13T15:38:05Z"), new Author(bob));
+        Comment c3 = new Comment("Tenha um ótimo dia!", Instant.parse("2021-02-14T12:34:26Z"), new Author(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().add(c3);
+
+        postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
